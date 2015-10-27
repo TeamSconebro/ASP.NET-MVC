0073381ff2d9e73f46a643d6bae64ae893d334c0
Bozhidar83 <b.chakurov@abv.bg>
2015-10-27 15:28:36 +0200

    Convert picture helpers

--- PhotoContest/PhotoContest.Web/Controllers/PicturesController.cs
+++ PhotoContest/PhotoContest.Web/Controllers/PicturesController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using PhotoContest.Data.UnitsOfWork;
+using PhotoContest.Web.Models.BindingModel;
 
 namespace PhotoContest.Web.Controllers
 {
@@ -19,6 +21,20 @@ namespace PhotoContest.Web.Controllers
             //TODO: Return view model with all pictures in one particular contest. Model should contain picture title, content (image) and author. That model should be rendered on the Contest details page.
 
             return View();
+        }
+
+        public ActionResult UploadImage(ImageBindingModel model)
+        {
+            var ownerId = this.User.Identity.GetUserId();
+            var owner = this.Data.Users.Find(ownerId);
+            if (owner == null)
+            {
+                return HttpNotFound();
+            }
+
+
+
+            return this.View();
         }
 
     }
---
+++ PhotoContest/PhotoContest.Web/Models/BindingModel/ImageBindingModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PhotoContest.Web.Models.BindingModel
+{
+    public class ImageBindingModel
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Base64Data { get; set; }
+    }
+}
