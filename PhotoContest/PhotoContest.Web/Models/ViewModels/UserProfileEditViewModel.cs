c2d0bc5269b44d1dcc276c1803cd899521cb23df
Todorov96 <todorov.georgi9@abv.bg>
2015-10-27 19:42:53 +0200

    UserEditViewModel

---
+++ PhotoContest/PhotoContest.Web/Models/ViewModels/UserProfileEditViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace PhotoContest.Web.Models.ViewModels
+{
+    public class UserEditProfileViewModel
+    {
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
