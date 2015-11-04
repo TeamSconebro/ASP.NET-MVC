e341f5293615a3bf017d30d8a4107ccd9f6225c4
Todorov96 <todorov.georgi9@abv.bg>
2015-11-04 23:43:41 +0200

    adminAreaToDO

---
+++ PhotoContest/PhotoContest.Web/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,22 @@
+﻿
+
+//namespace PhotoContest.Web.Areas.Admin.Controllers
+//{
+//    using System.Web.Mvc;
+//    using Data.UnitsOfWork;
+//    [Authorize(Roles = "Administrator")]
+//    public class BaseAdminController : Controller
+//    {
+//        public BaseAdminController()
+//            : this(new PhotoContestData())
+//        {
+//        }
+
+//        public BaseAdminController(IPhotoContestData data)
+//        {
+//            this.Data = data;
+//        }
+
+//        public IPhotoContestData Data { get; set; }
+//    }
+//}
