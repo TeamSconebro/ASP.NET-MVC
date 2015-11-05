d8d7ddc1508e763de0464a00c1bb986d199a5a74
Todorov96 <todorov.georgi9@abv.bg>
2015-11-05 17:08:07 +0200

    errorHandling

---
+++ PhotoContest/PhotoContest.Web/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoContest.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        [ActionName("404")]
+        public ActionResult NotFound()
+        {
+            Response.TrySkipIisCustomErrors = true;
+            Response.StatusCode = 404;
+            return this.View("Error");
+        }
+    }
+}
